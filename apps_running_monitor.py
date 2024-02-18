e2816c96925954a3c0b9851995459218da63b1bd
Mykhailo Khranovskyi <mykhailo.khranovskyi@djangostars.com>
2024-02-18 23:00:15 +0200

    adjust time

--- apps_running_monitor.py
+++ apps_running_monitor.py
@@ -2,7 +2,7 @@ import subprocess
 import telebot
 import schedule
 import time
-from config import TOKEN, YOUR_CHAT_ID  # Assuming you have a file named config.py with your Telegram bot token
+from config import TOKEN, YOUR_CHAT_ID
 
 # Initialize the bot
 bot = telebot.TeleBot(TOKEN)
@@ -32,24 +32,27 @@ def check_bots():
         print("Not all bots are up and running!")
 
 
-
 def send_daily_status():
-    print("Daily status check: All bots are up and running!")
-    bot.send_message(chat_id=YOUR_CHAT_ID, text="Daily status check: All bots are up and running!")
+    check_bots()
+    running_bots = "\n".join(BOT_SCRIPTS)
+    status_message = f"Daily status check: Monitored bots/apps are:\n{running_bots}"
+    bot.send_message(chat_id=YOUR_CHAT_ID, text=status_message)
 
 
 def main():
+    send_daily_status()
     # Schedule bot check every minute
     schedule.every().minute.do(check_bots)
 
     # Schedule status report every 5 minutes
-    schedule.every(3).minutes.do(send_daily_status)
+    # schedule.every(3).minutes.do(send_daily_status)
 
     # Schedule bot check every hour
-#    schedule.every().hour.do(check_bots)
+    # schedule.every().hour.do(check_bots)
 
     # Schedule daily status message
-#    schedule.every().day.at("09:00").do(send_daily_status)  # Adjust the time as needed
+    schedule.every().day.at("07:00").do(send_daily_status)  # Adjust the time as needed
+    schedule.every().day.at("17:00").do(send_daily_status)  # Adjust the time as needed
 
     # Infinite loop to keep the program running
     while True:
